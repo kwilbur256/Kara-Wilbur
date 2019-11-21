9ceeaeb4d0c634bb7e3499c19786a488cf477f3f
kwilbur256 <kwilbur@mymail.mines.edu>
2019-11-21 01:00:18 -0700

    Function 4

--- Week12-utility.py
+++ Week12-utility.py
@@ -10,6 +10,12 @@ def LoadFile(file):
     contents = text.read()
     text.close()
     print("OUTPUT", contents)
+    
 def UpdateString(str_one, str_two, index):
     str_one = str_one[:index] + str_two + str_one[index + 1:]
     print("OUTPUT", str_one)
+
+def FindWordCoUnt(lst , string):
+    number = lst.count(string)
+    return number
+
