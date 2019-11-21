aae3756c59f256d361ec31949c9b7ca40fcd4eeb
kwilbur256 <kwilbur@mymail.mines.edu>
2019-11-21 00:54:02 -0700

    Function 3

--- Week12-utility.py
+++ Week12-utility.py
@@ -3,10 +3,13 @@
 #Week 12 LabA
 
 def PrintOutput(output):
-    print("OUTPUT", output)git
+    print("OUTPUT", output)
 
 def LoadFile(file):
     text = open(file, "r")
     contents = text.read()
     text.close()
     print("OUTPUT", contents)
+def UpdateString(str_one, str_two, index):
+    str_one = str_one[:index] + str_two + str_one[index + 1:]
+    print("OUTPUT", str_one)
